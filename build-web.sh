70119673c4207729f92299c95ab7065086dccf78
Tatu Heikkilä <public@tazca.com>
2024-09-26 20:10:15 +0300

    Use canvaskit on post-iOS15 devices to fix SVG rendering

--- build-web.sh
+++ build-web.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 flutter clean
 dart run flutter_native_splash:create
-flutter build web --dart-define=FLUTTER_WEB_CANVASKIT_URL=/canvaskit/
+flutter build web --dart-define=FLUTTER_WEB_CANVASKIT_URL=/canvaskit/ --web-renderer=auto
